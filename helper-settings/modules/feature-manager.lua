6c43486eb20473e917c43040a00ba29f420e1908
xhub-ui <xukrosthub@gmail.com>
2025-12-11 10:29:28 +0700

    chore: upload extracted helper-settings/modules/feature-manager.lua

--- helper-settings/modules/feature-manager.lua
+++ helper-settings/modules/feature-manager.lua
@@ -10,7 +10,7 @@ local HttpService = game:GetService("HttpService")
 local player = Players.LocalPlayer
 
 -- [[ KONFIGURASI LINK ]] --
-local LOGGER_URL = "https://raw.githubusercontent.com/xhub-ui/x-loaders-directly/main/helper-settings/logger.lua"
+local LOGGER_URL = "https://raw.githubusercontent.com/xhub-ui/x-loaders-directly/refs/heads/main/helper-settings/logger.lua"
 
 -- Safe Load Logger
 local success, Logger = pcall(function() return loadstring(game:HttpGet(LOGGER_URL))() end)
@@ -25,11 +25,11 @@ FeatureManager.IsAutoTeleporting = false
 FeatureManager.ScriptLibrary = {
     -- [MAPS]
     ["Mount Blonde"] = {
-        Url = "https://raw.githubusercontent.com/xhub-ui/x-loaders-directly/main/main-maps/mt-coor-scripts/Coor-Mt-Blonde.lua",
+        Url = "https://raw.githubusercontent.com/xhub-ui/x-loaders-directly/refs/heads/main/main-maps/mt-coor-scripts/Coor-Mt-Blonde.lua",
         VipOnly = false
     },
     ["Mount Sakahayang"] = {
-        Url = "https://raw.githubusercontent.com/xhub-ui/x-loaders-directly/main/main-maps/mt-coor-scripts/Coor-Mt-Sakahayang.lua", 
+        Url = "https://raw.githubusercontent.com/xhub-ui/x-loaders-directly/refs/heads/main/main-maps/mt-coor-scripts/Coor-Mt-Sakahayang.lua", 
         VipOnly = false
     },
     ["Mount Malang (VIP)"] = {
