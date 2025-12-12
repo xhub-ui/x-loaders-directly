917ff61c02bd9c600eaf3078092eda2c1cab97f0
xhub-ui <xukrosthub@gmail.com>
2025-12-12 15:30:18 +0700

    upload music-list.lua

---
+++ main-settings/direct-ui-settings/music-list.lua
@@ -0,0 +1,11 @@
+-- music-list.lua
+return {
+    {Name = "Ncik - Spongebob", Artist = "Ncik", ID = "15892557766"},
+    {Name = "DJ Pota Pota", Artist = "Tiktok Viral", ID = "6076986950"},
+    {Name = "Metamorphosis", Artist = "Interworld", ID = "9066667954"},
+    {Name = "Duka", Artist = "Last Child", ID = "14238776883"},
+    {Name = "Glimpse of Us", Artist = "Joji", ID = "9829873045"},
+    {Name = "Resonance", Artist = "Home", ID = "5304629731"},
+    {Name = "Cupid", Artist = "Fifty Fifty", ID = "13123838478"},
+    {Name = "Komang", Artist = "Raim Laode", ID = "12564246949"}
+}
